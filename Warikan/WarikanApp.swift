d78628ffa1aae007e3704bc0bc866847199199d4
Saikaku-69 <grnmnsta@gmail.com>
2025-03-01 18:40:18 +0900

    Add Files

---
+++ Warikan/WarikanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarikanApp.swift
+//  Warikan
+//
+//  Created by cmStudent on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct WarikanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
